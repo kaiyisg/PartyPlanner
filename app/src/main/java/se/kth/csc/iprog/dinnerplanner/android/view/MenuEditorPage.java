bd1d7269d64a2d685f9f893d710c9a9390d17f20
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-02-17 14:54:54 +0100

    partial implementation of menueditorpage populating of listview

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/MenuEditorPage.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/MenuEditorPage.java
@@ -4,10 +4,19 @@ package se.kth.csc.iprog.dinnerplanner.android.view;
  * Created by Lee Han Young on 17-Feb-16.
  */
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Menus;
 
 
 public class MenuEditorPage {
@@ -24,9 +33,17 @@ public class MenuEditorPage {
         ListView allCurrentMenus = (ListView) view.findViewById(R.id.listView4);
         TextView currentMenu = (TextView) view.findViewById(R.id.textView14);
 
-        //populating the list view based on the number of items in my dinner model
+        Set<Menus> allMenuData = DinnerModel.getAllMenus();
 
-        //model.
+
+        //getting string array of menu name data
+        List<String> menuViewData = new ArrayList<String>();
+        Iterator<Menus> iter = allMenuData.iterator();
+        while(iter.hasNext()){
+            menuViewData.add(iter.next().getMenuName());
+        }
+        //ListAdapter list = new ArrayAdapter<String>(this,R.id.listView4,menuViewData);
+        //allMenuData.
 
     }
 
--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/model/Menus.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/model/Menus.java
@@ -8,18 +8,21 @@ import java.util.Set;
  */
 public class Menus {
 
-    private static Set<Dish> dishes = new HashSet<Dish>();
+    private Set<Dish> dishes = new HashSet<Dish>();
+    private String menuName;
 
     //// TODO: 17-Feb-16 implement stricter checks for adding a menu, see if can add menu so easily
 
-    public Menus (Dish dish1, Dish dish2, Dish dish3){
+    public Menus (String menuname, Dish dish1, Dish dish2, Dish dish3){
+        this.menuName = menuName;
         dishes.add(dish1);
         dishes.add(dish2);
         dishes.add(dish3);
     }
 
-    public static Set<Dish> getAllDishes(){
+    public Set<Dish> getAllDishes(){
         return dishes;
     }
+    public String getMenuName(){return menuName;}
 
 }
